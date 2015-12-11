a97f1f3b8d1dc32bb3784cf7f936107b6ff5a6ec
jan-tee <jan.tietze@gmail.com>
2015-12-11 03:29:10 -0500

    Ensure compatibility with authentication through ADFS proxy and let the ADFS side choose the authentication method, and accept any.

--- phpsaml/onelogin/saml/authrequest.php
+++ phpsaml/onelogin/saml/authrequest.php
@@ -34,8 +34,6 @@ class SamlAuthRequest {
             "<samlp:AuthnRequest xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" ID=\"$id\" Version=\"2.0\" IssueInstant=\"$issue_instant\" ProtocolBinding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\" AssertionConsumerServiceURL=\"".$this->settings->assertion_consumer_service_url."\">".
             "<saml:Issuer xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">".$this->settings->issuer."</saml:Issuer>\n".
             "<samlp:NameIDPolicy xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" Format=\"".$this->settings->name_identifier_format."\" AllowCreate=\"true\"></samlp:NameIDPolicy>\n".
-            "<samlp:RequestedAuthnContext xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" Comparison=\"exact\">".
-            "<saml:AuthnContextClassRef xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">urn:federation:authentication:windows</saml:AuthnContextClassRef></samlp:RequestedAuthnContext>\n".
             "</samlp:AuthnRequest>";
 
         $deflated_request  = gzdeflate($request);
