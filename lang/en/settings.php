cbbc66db69d0e3aaeeaf54d9297bf47cdd628fac
Andreas Gohr <gohr@cosmocode.de>
2018-05-23 15:15:01 +0200

    adjusted default user attribute and added translations

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -3,6 +3,13 @@
  * Configuration texts for ADFS SAML Auth
  */
 
-$conf['endpoint']    = 'The SAML auth API endpoint. Usually <code>https://<i>&lt;yourserver&gt;</i>/adfs/ls/</code>';
-$conf['certificate'] = 'The SAML auth certificate';
+$lang['endpoint']    = 'The SAML auth API endpoint. Usually <code>https://<i>&lt;yourserver&gt;</i>/adfs/ls/</code>';
+$lang['certificate'] = 'The SAML auth certificate';
+$lang['lowercase'] = 'Treat user and group names as case insensitive and lower case them automatically?';
+$lang['autoprovisioning'] = 'Automatic user provisioning: authenticated users are created automatically and do not need to be added manually by the wiki administrator';
+$lang['userid_attr_name'] = 'User login name attribute';
+$lang['fullname_attr_name'] = 'Full name attribute';
+$lang['email_attr_name'] = 'E-mail attribute';
+$lang['groups_attr_name'] = 'Groups attribute';
+
 
